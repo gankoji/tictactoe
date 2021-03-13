3d67d2842c45365b82e42db7a25673211733ce1e
Jake Bailey <asaxplayinghorse@gmail.com>
2021-03-13 13:23:35 -0700

    Closing in on MVP. Working on win condition checking.

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,13 +1,6 @@
 (ns tictactoe.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-
-
 (defn checkRange
   "Checks that move inputs are on the board."
   [input]
@@ -32,13 +25,48 @@
     (let [column (getMoveInRange)]
       (vec [row column]))))
 
-(def gameBoard ["-" "-" "-" "-" "-" "-" "-" "-" "-"])
-(println gameBoard)
+
 (defn updateBoard
   [move currentBoard token]
   (let [[row col] move]
   (assoc currentBoard (+ (* 3 row) col) token)))
 
-(def firstMove (updateBoard (getUserMove) gameBoard "X"))
-(println firstMove)
-(println (updateBoard (getUserMove) firstMove "O"))
+(defn validateMove 
+  "This function takes a move input and the current gameboard, and returns a
+  validated move. It will recurse on itself until a valid move is found. "
+  [move gameBoard]
+  (let [[row col] move
+        curr (nth gameBoard (+ (* 3 row) col))]
+    (if (= 0 (compare curr "-"))
+     move
+     (do
+      (println "That spot is taken. Try again!")
+      (validateMove (getUserMove) gameBoard)))))
+
+(def threeInARow  [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
+
+(defn tokensMatch?
+  "Finds the tokens at matchPositions on the board. If all three are the
+   same, returns true, else false."
+  [playerToken matchPositions gameBoard]
+  (let [pieces (map #(nth % gameBoard) matchPositions)
+        truthy (map #(= % playerToken) pieces)]
+        (every? identity truthy)))
+
+(defn hasWon?
+  "Checks for the win condition"
+  [playerToken gameBoard]
+  (let [truthy (map #(tokensMatch? playerToken % gameBoard) threeInARow)]
+  (println truthy)
+  (any? truthy)))
+
+(def gameBoard ["-" "-" "-" "-" "-" "-" "-" "-" "-"])
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ;;(println (hasWon? "X" gameBoard))
+  (def firstMove (updateBoard (validateMove (getUserMove) gameBoard) gameBoard "X"))
+  (println firstMove)
+  (println (updateBoard (validateMove (getUserMove) firstMove) firstMove "O"))
+)
