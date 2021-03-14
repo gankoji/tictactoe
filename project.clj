a9686e33260f8bdf3b9008aef1b35f3da241da05
Jake Bailey <asaxplayinghorse@gmail.com>
2021-03-14 12:21:01 -0700

    Fixing config for uberjar and running without lein.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot tictactoe.core
+  :main tictactoe.core
+  :aot [tictactoe.core]
   :target-path "target/%s"
   :plugins [[cider/cider-nrepl "0.25.6"]]
   :profiles {:uberjar {:aot :all
